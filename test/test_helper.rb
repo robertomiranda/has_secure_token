9c1fe7ef365c67aa13a61680dfbe1545d638b990
Rémi Delhaye <remi.delhaye@epsi.fr>
2015-05-02 00:36:51 +0200

    Fix some syntax and add tests to README.md.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,10 +11,10 @@ require "minitest/unit"
 
 Dir["models/*.rb"].each {|file| require file }
 
-  def assert_not_nil(exp, msg=nil)
-        msg = message(msg) { "<#{mu_pp(exp)}> expected to not be nil" }
-        assert(!exp.nil?, msg)
-  end
+def assert_not_nil(exp, msg=nil)
+  msg = message(msg) { "<#{mu_pp(exp)}> expected to not be nil" }
+  assert(!exp.nil?, msg)
+end
 
 DB_FILE = 'tmp/test_db'
 FileUtils.mkdir_p File.dirname(DB_FILE)
